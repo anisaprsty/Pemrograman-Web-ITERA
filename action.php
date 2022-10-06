33362f05a93ca113c5ebcc6b9740841c4695361d
Anisa Prasetya <anisaprsty@gmail.com>
2022-10-07 00:12:41 +0700

    Add files via upload

---
+++ action.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Product Detail</title>
+
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="imgBx">
+            <td><center><img src="https://user-images.githubusercontent.com/100473334/194367872-6c876fbc-f1e0-4097-b11c-a8dde0d2b35a.jpg" width="350" height="400" /></center><center><b>ID 101 - lighstick BTS Official</b></center></td>
+        </div>
+        <div class="details">
+            <div class="content">
+                <h2>Lighstick BTS Official 5.0 <br>
+                    <span>Butter Collection</span>
+                </h2>
+                <p>
+                The BTS official light stick (ver. 3) was introduced when the teasers came out on July 5,
+                2018. It was officially released on July 27, 2018. Similarly to the previous version it can be
+                controlled by a central hub via Bluetooth connection at the concerts of BTS. Additionally
+                it can be also controlled via special app that's available on the App Store for iPhone and
+                on Google Play for Android smartphones. The item was designed, manufactured and
+                distributed by Compact.D company from South Korea and officially licensed by Big Hit
+                Entertainment. The model number of the light stick (ver.3) is BTS×Official 7118G. It comes
+                with a sticker of authenticity on the package.
+
+                <h3>Rp. 500.000,00</h3>
+                <button>Buy Now</button>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
